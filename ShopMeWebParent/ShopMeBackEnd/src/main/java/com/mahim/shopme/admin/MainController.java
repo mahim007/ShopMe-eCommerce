d4f2c09b165019031639e25d139fdf0e6d645dfa
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-12-11 22:05:20 +0600

    initial commit for project structure setup

---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/mahim/shopme/admin/MainController.java
@@ -0,0 +1,12 @@
+package com.mahim.shopme.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+}
