0eac33ecaaa309ba03ff68944b80f175306a13c6
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2024-04-05 16:40:01 +0600

    Customer entity and repository

---
+++ ShopMeCommon/src/main/java/com/mahim/shopme/common/entity/Customer.java
@@ -0,0 +1,62 @@
+package com.mahim.shopme.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name" ,nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false, length = 45)
+    private String phoneNumber;
+
+    @Column(name = "address_line1", nullable = false, length = 64)
+    private String addressLine1;
+
+    @Column(name = "address_line2", nullable = true, length = 64)
+    private String addressLine2;
+
+    @Column(nullable = false, length = 45)
+    private String city;
+
+    @Column(nullable = false, length = 45)
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(name = "postal_code", nullable = false, length = 10)
+    private String postalCode;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column
+    private boolean enabled;
+
+    @Column(name = "verification_code", length = 64)
+    private String verificationCode;
+}
---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/java/com/mahim/shopme/admin/customer/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.mahim.shopme.admin.customer;
+
+import com.mahim.shopme.common.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    Customer findByEmail(String email);
+    Customer findByVerificationCode(String code);
+
+    @Query("UPDATE Customer c SET c.enabled = true WHERE c.id = ?1")
+    @Modifying
+    void enable(Integer id);
+}
