3f9407142317355b04eaf4ab067742c289b7af41
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2026-01-16 01:56:56 +0600

    code refactored for sales reporting

--- ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/reports/sales_report_by_date.js
+++ ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/reports/sales_report_by_date.js
@@ -5,84 +5,23 @@ let chartOptions;
 let totalGrossSales;
 let totalNetSales;
 let totalOrders;
-let startDateField;
-let endDateField;
-
-function initCustomDateRange() {
-    startDateField = $("#startDate");
-    endDateField = $("#endDate");
-
-    let endDate = new Date();
-    endDateField.val(endDate.toISOString().split('T')[0]);
-
-    let startDate = new Date();
-    startDate.setDate(endDate.getDate() - 30);
-    startDateField.val(startDate.toISOString().split('T')[0]);
-}
-
-function getNumberOfDays() {
-    let startDate = new Date(startDateField.val());
-    let endDate = new Date(endDateField.val());
-
-    let differenceInMillis = endDate.getTime() - startDate.getTime();
-    return differenceInMillis / (1000 * 3600 * 24);
-}
-
-function validateDateRange() {
-    let differenceInDays = getNumberOfDays();
-    if (differenceInDays >= 1 && differenceInDays <= 30) {
-        startDateField[0].setCustomValidity("");
-        return true;
-    } else {
-        startDateField[0].setCustomValidity("Date range must be between 1 and 30 days");
-        startDateField[0].reportValidity();
-        return false;
-    }
-}
 
 $(document).ready(function () {
-    $(".button_sales_by_date").on("click", function () {
-        if ($(this).hasClass("btn-light")) {
-            $(".button_sales_by_date").each(function (index, element) {
-                $(element).removeClass("btn-primary").addClass("btn-light");
-            });
-
-            $(this).removeClass("btn-light").addClass("btn-primary");
-
-            let divCustomDateRange = $("#divCustomDateRange");
-            let period = $(this).attr("period");
-            if (period) {
-                divCustomDateRange.addClass('d-none')
-                loadSalesReportByDate(period);
-            } else {
-                divCustomDateRange.removeClass('d-none');
-            }
-        }
-    });
-
-    initCustomDateRange();
-    $("#btnViewReportByDateRange").on("click", function (e) {
-        e.preventDefault();
-        if (validateDateRange()) {
-            loadSalesReportByDate("custom")
-        }
-    });
+    setupEventHandlers("_date", loadSalesReportByDate);
 });
 
 function loadSalesReportByDate(period = "last_7_days") {
-    if (period === "custom") {
-        period = $("#startDate").val() + "/" + $("#endDate").val();
-    }
-
-    let requestURL = contextPath + "reports/sales_by_date/" + period;
+    let requestURL = contextPath + "reports/sales_by_date/" + getUrlSuffix(period, "_date");
     fetchData(requestURL, "GET", res => {
-        prepareChartData(res);
-        customizeChart(period);
-        drawChart(period);
+        prepareChartDataForSalesReportByDate(res);
+        customizeChartForSalesReportByDate(period);
+        formatChartData(data, 1, 2);
+        drawChartForSalesReportByDate(period);
+        setSalesAmount(period, "_date", "Total Orders:");
     }, error => console.log(error))
 }
 
-function prepareChartData(res) {
+function prepareChartDataForSalesReportByDate(res) {
     data = new google.visualization.DataTable();
     data.addColumn("string", "Date");
     data.addColumn("number", "Gross Sales");
@@ -101,7 +40,7 @@ function prepareChartData(res) {
     });
 }
 
-function customizeChart(period) {
+function customizeChartForSalesReportByDate(period) {
     chartOptions = {
         title: getChartTitle(period),
         height: 360,
@@ -116,54 +55,9 @@ function customizeChart(period) {
             1: {title: 'Number of Orders'},
         }
     };
-
-    let formatter = new google.visualization.NumberFormat({
-        prefix: prefixCurrencySymbol,
-        suffix: suffixCurrencySymbol,
-        decimalSymbol: decimalPointType,
-        groupingSymbol: thousandPointType,
-        fractionDigits: decimalDigits
-    });
-
-    formatter.format(data, 1); // format currency for gross sales column
-    formatter.format(data, 2); // format currency for net sales column
 }
 
-function drawChart(period) {
+function drawChartForSalesReportByDate() {
     let salesChart = new google.visualization.ColumnChart(document.getElementById("chart_sales_by_date"));
     salesChart.draw(data, chartOptions);
-
-    $("#textTotalGrossSales").text(formatCurrency(totalGrossSales));
-    $("#textTotalNetSales").text(formatCurrency(totalNetSales));
-    $("#textAvgGrossSales").text(formatCurrency(totalGrossSales / getDenominator(period)));
-    $("#textAvgNetSales").text(formatCurrency(totalNetSales / getDenominator(period)));
-    $("#textTotalOrders").text($.number(totalOrders));
-}
-
-function formatCurrency(amount) {
-    let formattedAmount = $.number(amount, decimalDigits, decimalPointType, thousandPointType);
-    return prefixCurrencySymbol + formattedAmount + suffixCurrencySymbol;
-}
-
-function getChartTitle(period) {
-    switch (period) {
-        case "last_7_days": return "Sales in Last 7 Days";
-        case "last_28_days": return "Sales in Last 28 Days";
-        case "last_6_months": return "Sales in Last 6 Moths";
-        case "last_12_months": return "Sales in Last 12 Months";
-        case "custom": return "Sales in Custom Date Range";
-    }
-
-    return "";
-}
-
-function getDenominator(period) {
-    switch (period) {
-        case "last_7_days": return 7;
-        case "last_28_days": return 28;
-        case "last_6_months": return 6;
-        case "last_12_months": return 12;
-        case "custom": return getNumberOfDays();
-        default: return 1;
-    }
 }
---
+++ ShopMeWebParent/ShopMeBackEnd/src/main/resources/static/js/reports/sales_report_common.js
@@ -0,0 +1,128 @@
+// Sales report common scripts
+
+function setupEventHandlers(reportType, callback) {
+    $(".button_sales_by" + reportType).on("click", function () {
+        if ($(this).hasClass("btn-light")) {
+            $(".button_sales_by" + reportType).each(function (index, element) {
+                $(element).removeClass("btn-primary").addClass("btn-light");
+            });
+
+            $(this).removeClass("btn-light").addClass("btn-primary");
+
+            let divCustomDateRange = $("#divCustomDateRange" + reportType);
+            let period = $(this).attr("period");
+            if (period) {
+                divCustomDateRange.addClass('d-none')
+                callback(period);
+            } else {
+                divCustomDateRange.removeClass('d-none');
+            }
+        }
+    });
+
+    initCustomDateRange(reportType);
+    $("#btnViewReportByDateRange" + reportType).on("click", function (e) {
+        e.preventDefault();
+        if (validateDateRange(reportType)) {
+            callback("custom");
+        }
+    });
+}
+
+function initCustomDateRange(reportType) {
+    let startDateField = $("#startDate" + reportType);
+    let endDateField = $("#endDate" + reportType);
+
+    let endDate = new Date();
+    endDateField.val(endDate.toISOString().split('T')[0]);
+
+    let startDate = new Date();
+    startDate.setDate(endDate.getDate() - 30);
+    startDateField.val(startDate.toISOString().split('T')[0]);
+}
+
+function getNumberOfDays(reportType) {
+    let startDateField = $("#startDate" + reportType);
+    let endDateField = $("#endDate" + reportType);
+
+    let startDate = new Date(startDateField.val());
+    let endDate = new Date(endDateField.val());
+
+    let differenceInMillis = endDate.getTime() - startDate.getTime();
+    return differenceInMillis / (1000 * 3600 * 24); // difference in days
+}
+
+function validateDateRange(reportType) {
+    let startDateField = $("#startDate" + reportType);
+    let endDateField = $("#endDate" + reportType);
+    let differenceInDays = getNumberOfDays(reportType);
+
+    if (differenceInDays >= 1 && differenceInDays <= 30) {
+        startDateField[0].setCustomValidity("");
+        return true;
+    } else {
+        startDateField[0].setCustomValidity("Date range must be between 1 and 30 days");
+        startDateField[0].reportValidity();
+        return false;
+    }
+}
+
+function formatCurrency(amount) {
+    let formattedAmount = $.number(amount, decimalDigits, decimalPointType, thousandPointType);
+    return prefixCurrencySymbol + formattedAmount + suffixCurrencySymbol;
+}
+
+function getChartTitle(period) {
+    switch (period) {
+        case "last_7_days": return "Sales in Last 7 Days";
+        case "last_28_days": return "Sales in Last 28 Days";
+        case "last_6_months": return "Sales in Last 6 Moths";
+        case "last_12_months": return "Sales in Last 12 Months";
+        case "custom": return "Sales in Custom Date Range";
+    }
+
+    return "";
+}
+
+function getDenominator(period, reportType) {
+    switch (period) {
+        case "last_7_days": return 7;
+        case "last_28_days": return 28;
+        case "last_6_months": return 6;
+        case "last_12_months": return 12;
+        case "custom": return getNumberOfDays(reportType);
+        default: return 1;
+    }
+}
+
+function setSalesAmount(period, reportType, labelTotalItems) {
+    $("#textTotalGrossSales" + reportType).text(formatCurrency(totalGrossSales));
+    $("#textTotalNetSales" + reportType).text(formatCurrency(totalNetSales));
+    $("#textAvgGrossSales" + reportType).text(formatCurrency(totalGrossSales / getDenominator(period, reportType)));
+    $("#textAvgNetSales" + reportType).text(formatCurrency(totalNetSales / getDenominator(period, reportType)));
+    $("#labelTotalItems" + reportType).text(labelTotalItems);
+    $("#textTotalItems" + reportType).text($.number(totalOrders));
+}
+
+function formatChartData(data, colIndex1, colIndex2) {
+    let formatter = new google.visualization.NumberFormat({
+        prefix: prefixCurrencySymbol,
+        suffix: suffixCurrencySymbol,
+        decimalSymbol: decimalPointType,
+        groupingSymbol: thousandPointType,
+        fractionDigits: decimalDigits
+    });
+
+    formatter.format(data, colIndex1); // format currency for gross sales column
+    formatter.format(data, colIndex2); // format currency for net sales column
+}
+
+function getUrlSuffix(period, reportType) {
+    if (period === "custom") {
+        let startDateField = $("#startDate" + reportType);
+        let endDateField = $("#endDate" + reportType);
+        return startDateField.val() + "/" + endDateField.val();
+    }
+
+    return period;
+}
